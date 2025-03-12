aa32d03794f9f13e1e7b2629792a6d0dee337958
fahad111000 <fahad.kh2210@gmail.com>
2025-03-13 04:11:34 +0500

    image is added and modify some desinges

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 let cursor = document.getElementById("cursor");
 let main = document.getElementById('main');
+let image = document.getElementById('image');
+
 
 main.addEventListener("mousemove", (e) => {
     gsap.to(cursor, {
         x: e.x,
         y: e.y,
         duration: 0.5
+    })
+})
+
+// Mouse enter on image
+image.addEventListener("mouseenter", () => {
+    gsap.to(cursor, {
+        scale: 2,
+        backgroundColor: "hsla(0, 3%, 78%, 0.596)"
+    })
+})
+
+// Mouse out from image
+image.addEventListener("mouseleave", () => {
+    gsap.to(cursor, {
+        scale: 1
     })
 })
